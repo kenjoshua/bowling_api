ed74f2de1bba6a4773cba60161f6778c454d68b3
Agero-Jerwin Darrel <72547953+Agero-JerwinDarrel@users.noreply.github.com>
2022-02-06 22:27:41 +0800

    Added Repo

---
+++ BowlingAPI/Repositories/FrameRepository.cs
@@ -0,0 +1,49 @@
+﻿using BowlingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingAPI.Repositories
+{
+    public class FrameRepository : IFrameRepository
+    {
+        private readonly FrameContxt _context;
+
+        public FrameRepository(FrameContxt context)
+        {
+            this._context = context;
+        }
+        public async Task<Frame> Create(Frame frame)
+        {
+            _context.Frames.Add(frame);
+            await _context.SaveChangesAsync();
+
+            return frame;
+        }
+
+        public async Task Delete(int id)
+        {
+            var frameToDelete = await _context.Frames.FindAsync(id);
+            _context.Frames.Remove(frameToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Frame>> Get()
+        {
+            return await _context.Frames.ToListAsync();
+        }
+
+        public async Task<Frame> Get(int id)
+        {
+            return await _context.Frames.FindAsync(id);
+        }
+
+        public async Task Update(Frame frame)
+        {
+            _context.Entry(frame).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ BowlingAPI/Repositories/IFrameRepository.cs
@@ -0,0 +1,18 @@
+﻿using BowlingAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingAPI.Repositories
+{
+    public interface IFrameRepository
+    {
+        Task<IEnumerable<Frame>> Get();
+        Task<Frame> Get(int id);
+
+        Task<Frame> Create(Frame frame);
+        Task Update(Frame frame);
+        Task Delete(int id);
+    }
+}
